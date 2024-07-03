9f1eee9b3d0f8101420407acbbbdae537a71b153
Andrm <myasnikovas@dipaul.ru>
2024-07-03 12:28:40 +0300

    Parallelism HW

---
+++ EntityFW_ConsoleApp/Parallelism/Classes/FileWorker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallelism.Classes
+{
+    public static class FileWorker
+    {
+        public static async Task<string> ReadFile(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        public static async Task<int> CalculateSpaces(string text)
+        {
+            return text.Count(el => el == ' ');
+        }
+
+        public static async Task<int> ReadFileAndCalculateSpaces(string filePath)
+        {
+            string text = await ReadFile(filePath);
+            return await CalculateSpaces(text);
+        }
+
+        public static async Task<int> ReadFilesFromDirectoryAndCalculateSpaces(string dirPath)
+        {
+            var filesInDirectory = Directory.GetFiles(dirPath, "*.*", SearchOption.TopDirectoryOnly);
+
+            int totalSpaces = 0;
+
+            Parallel.ForEach(filesInDirectory, file =>
+            {
+                int spacesInFile = ReadFileAndCalculateSpaces(file).Result;
+                Console.WriteLine(@"Total spaces in file {0} is {1}", file, spacesInFile);
+                totalSpaces += spacesInFile;
+            });
+
+            Console.WriteLine(@"Total spaces in files of direactory {0} is {1}", dirPath, totalSpaces);
+            return totalSpaces;
+        }
+    }
+}
---
+++ EntityFW_ConsoleApp/Parallelism/Program.cs
@@ -0,0 +1,37 @@
+﻿using Parallelism.Classes;
+using System.Diagnostics;
+
+//used https://onlinefiletools.com/generate-random-text-file to generate 10 mB files of text
+string filePath1 = @"C:\SearchFiles\1.txt";
+string filePath2 = @"C:\SearchFiles\2.txt";
+string filePath3 = @"C:\SearchFiles\3.txt";
+
+Stopwatch stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+Task t1 = new Task(() => Console.WriteLine(@"file: {0} contains {1} spaces", filePath1, FileWorker.ReadFileAndCalculateSpaces(filePath1).Result));
+Task t2 = new Task(() => Console.WriteLine(@"file: {0} contains {1} spaces", filePath2, FileWorker.ReadFileAndCalculateSpaces(filePath2).Result));
+Task t3 = new Task(() => Console.WriteLine(@"file: {0} contains {1} spaces", filePath3, FileWorker.ReadFileAndCalculateSpaces(filePath3).Result));
+
+t1.Start();
+//await t1;
+t2.Start();
+//await t2;
+t3.Start();
+//await t3;
+
+await Task.WhenAll(t1, t2, t3);
+//it takes more than 300 milliseconds for consecutive case and close to 200 milliseconds for parallel case
+
+stopwatch.Stop();
+Console.WriteLine(@"Time it took: {0} milliseconds", stopwatch.ElapsedMilliseconds);
+
+string directoryPath = @"C:\SearchFiles";
+
+stopwatch.Reset();
+stopwatch.Start();
+await FileWorker.ReadFilesFromDirectoryAndCalculateSpaces(directoryPath);
+stopwatch.Stop();
+
+Console.WriteLine(@"Time it took: {0} milliseconds", stopwatch.ElapsedMilliseconds);
